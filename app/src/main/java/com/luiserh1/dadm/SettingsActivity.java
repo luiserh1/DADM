4873e245fe6b9b157baa037b13a6e934cd6c4f8f
luiserh1 <54946908+luiserh1@users.noreply.github.com>
2020-03-01 23:23:30 +0100

    Practica 3: Ejercicio 02

--- app/src/main/java/com/luiserh1/dadm/SettingsActivity.java
+++ app/src/main/java/com/luiserh1/dadm/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.luiserh1.dadm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,10 +16,16 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settingsLayout, new SettingsFragment())
+                .commit();
+
+
+        /*SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         EditText et = findViewById(R.id.etName);
         String userName = prefs.getString("username", "");
-        et.setText(userName);
+        et.setText(userName);*/
     }
 
     @Override
@@ -27,12 +34,20 @@ public class SettingsActivity extends AppCompatActivity {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = prefs.edit();
 
-        EditText et = findViewById(R.id.etName);
+
+        /*EditText et = findViewById(R.id.etName);
         if (et != null && et.getText().toString().length() > 0) {
             editor.putString("username", et.getText().toString());
         } else {
             editor.remove("username");
         }
-        editor.apply();
+        editor.apply();*/
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences_settings, rootKey);
+        }
     }
 }
